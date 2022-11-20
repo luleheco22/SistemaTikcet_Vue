bc9f73532cfc036956a1f39f7c942f031a232049
leonel <leonelhcontreras@gmail.com>
2022-11-20 16:19:01 -0400

    ajuste de URL del back

--- sistemavue/src/main.js
+++ sistemavue/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import axios from 'axios'
 Vue.config.productionTip = false
+//Railway 
 axios.defaults.baseURL='https://backsustematicket-production.up.railway.app/'
 
 new Vue({
